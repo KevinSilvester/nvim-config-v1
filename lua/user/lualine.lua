986efc10b64dd1ce3d93e6af288bba941f15d488
Kevin Silvester <kevinsilvester05@gmail.com>
2022-08-22 21:39:59 +0100

    updated lualine + lsp

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,9 +3,32 @@ if not status_ok then
 	return
 end
 
+local function diff_source()
+	local gitsigns = vim.b.gitsigns_status_dict
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed,
+		}
+	end
+end
+
+local colors = {
+	green = "#98be65",
+	red = "#ec5f67",
+	yellow = "#ECBE7B",
+}
+
 local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
+
+local branch = {
+	"b:gitsigns_head",
+	icon = " ",
+	color = { gui = "bold" },
+}
 
 local diagnostics = {
 	"diagnostics",
@@ -18,9 +41,13 @@ local diagnostics = {
 
 local diff = {
 	"diff",
-	colored = false,
-	symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
-	cond = hide_in_width,
+	source = diff_source,
+	symbols = { added = "  ", modified = " ", removed = " " },
+	diff_color = {
+		added = { fg = colors.green },
+		modified = { fg = colors.yellow },
+		removed = { fg = colors.red },
+	},
 }
 
 local filetype = {
@@ -31,6 +58,44 @@ local filetype = {
 local location = {
 	"location",
 	padding = 0,
+}
+
+local treesitter = {
+	function()
+		return ""
+	end,
+	color = function()
+		local buf = vim.api.nvim_get_current_buf()
+		local ts = vim.treesitter.highlighter.active[buf]
+		return { fg = ts and not vim.tbl_isempty(ts) and colors.green or colors.red }
+	end,
+}
+
+local lsp = {
+	function(msg)
+		msg = msg or "LS Inactive"
+		local buf_clients = vim.lsp.buf_get_clients()
+		if next(buf_clients) == nil then
+			-- TODO: clean up this if statement
+			if type(msg) == "boolean" or #msg == 0 then
+				return "LS Inactive"
+			end
+			return msg
+		end
+		local buf_ft = vim.bo.filetype
+		local buf_client_names = {}
+
+		-- add client
+		for _, client in pairs(buf_clients) do
+			if client.name ~= "null-ls" then
+				table.insert(buf_client_names, client.name)
+			end
+		end
+
+		local unique_client_names = vim.fn.uniq(buf_client_names)
+		return "[" .. table.concat(unique_client_names, ", ") .. "]"
+	end,
+	color = { gui = "bold" },
 }
 
 local spaces = function()
@@ -49,9 +114,9 @@ lualine.setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { diagnostics },
-		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_b = { branch, "filename" },
+		lualine_c = { diff, diagnostics },
+		lualine_x = { treesitter, lsp, "encoding", filetype },
 		lualine_y = { location },
 		lualine_z = { "progress" },
 	},
