0576065372b56f1ae1784735159965dd8fa659d7
KevinSilvester <kevinsilvester05@gmail.com>
2022-08-23 13:10:54 +0100

    added guifont option

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,7 +34,7 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = true                             -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.guifont = { "JetBrainsMono NF", ":h9" }              -- the font used in graphical neovim applications
 vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
