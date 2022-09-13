b9b07b9fe25ffafe65a7d0b018312c52c2d57465
KevinSilvester <kevinsilvester05@gmail.com>
2022-09-13 23:31:23 +0100

    feat(lsp): updated powershell lsp options

--- lua/user/lsp/settings/powershell_es.lua
+++ lua/user/lsp/settings/powershell_es.lua
@@ -1,6 +1,33 @@
 local opts = {}
 
-if vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16")then
+local IS_WINDOWS = vim.loop.os_uname().sysname == "Windows_NT"
+
+local function bundle_path(path)
+   path = path or ""
+
+   local bp = "/mason/packages/powershell-editor-services/PowerShellEditorServices" .. path
+   if IS_WINDOWS then
+      bp = vim.fn.substitute(bp, "/", "\\", "g")
+   end
+
+   return vim.fn.stdpath("data") .. bp
+end
+
+local function log_path(file_name)
+   file_name = IS_WINDOWS and vim.fn.substitute(file_name, "/", "\\", "g") or file_name
+   return vim.fn.stdpath("cache") .. file_name
+end
+
+local pses_command = bundle_path("/Start-EditorServices.ps1")
+   .. " -BundledModulesPath "
+   .. bundle_path()
+   .. " -LogPath "
+   .. log_path("/pses.log")
+   .. " -SessionDetailsPath "
+   .. log_path("/pses-session.json")
+   .. " -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal"
+
+if vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16") then
    opts = {
       bundle_path = "C:/Users/kevin/lsp/PSES",
       cmd = {
@@ -8,7 +35,7 @@ if vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16")then
          "-NoLogo",
          "-NoProfile",
          "-Command",
-         "$env:HOMEPATH/lsp/PSES/Start-EditorServices.ps1 -BundledModulesPath $env:HOMEPATH/lsp/PSES -LogPath $env:TMP/logs.log -SessionDetailsPath $env:TMP/session.json -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal",
+         pses_command,
       },
    }
 end
